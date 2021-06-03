feb89c5f64fb8fa245688e410a0c66e6b2ae4f03
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-03 10:11:27 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner | Date: 03/06/2021
+		//Last edition: 
+		
+		
+	}
+}
